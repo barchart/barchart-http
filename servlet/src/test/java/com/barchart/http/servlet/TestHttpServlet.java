9158decab0be03cfb485ace92652876c7a1614ac
Andrei Pozolotin <Andrei.Pozolotin@gmail.com>
2013-08-02 13:03:58 -0500

    disable tests

--- servlet/src/test/java/com/barchart/http/servlet/TestHttpServlet.java
+++ servlet/src/test/java/com/barchart/http/servlet/TestHttpServlet.java
@@ -7,7 +7,7 @@
  */
 package com.barchart.http.servlet;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 
 import java.io.File;
@@ -30,7 +30,11 @@ import com.barchart.http.server.HttpServer;
 import com.barchart.http.server.HttpServerConfig;
 import com.barchart.util.test.junit.MultiThreadedRunner;
 
-// MJS: We run it in parallel to stress the server more and also to take advantage of multiple cores for speed
+/**
+ * MJS: We run it in parallel to stress the server more and also to take
+ * advantage of multiple cores for speed
+ */
+@Ignore
 @RunWith(MultiThreadedRunner.class)
 public class TestHttpServlet {
 
@@ -49,28 +53,27 @@ public class TestHttpServlet {
 		server = new HttpServer();
 
 		// MJS: Best way to access a resource for unit testing I found so far
-		final URL url =
-				this.getClass().getResource("barchart-servlet-example.war");
+		final URL url = this.getClass().getResource(
+				"barchart-servlet-example.war");
 
 		final File f = new File(url.getFile().replace("%20", " "));
 
-		servletSync =
-				new TestServlet(f, "com.barchart.servlet.example.TestServlet");
+		servletSync = new TestServlet(f,
+				"com.barchart.servlet.example.TestServlet");
 
-		servletAsync =
-				new TestServlet(f, "com.barchart.servlet.example.AsyncServlet");
+		servletAsync = new TestServlet(f,
+				"com.barchart.servlet.example.AsyncServlet");
 
 		port = 50000;
 
-		final HttpServerConfig config =
-				new HttpServerConfig()
-						.address(new InetSocketAddress("localhost", port))
-						.parentGroup(new NioEventLoopGroup(1))
-						.childGroup(new NioEventLoopGroup(1))
+		final HttpServerConfig config = new HttpServerConfig()
+				.address(new InetSocketAddress("localhost", port))
+				.parentGroup(new NioEventLoopGroup(1))
+				.childGroup(new NioEventLoopGroup(1))
 
-						// MJS: Attach the servlets
-						.requestHandler("/servlet", servletSync)
-						.requestHandler("/servletAsync", servletAsync);
+				// MJS: Attach the servlets
+				.requestHandler("/servlet", servletSync)
+				.requestHandler("/servletAsync", servletAsync);
 
 		server.configure(config).listen().sync();
 
@@ -88,8 +91,7 @@ public class TestHttpServlet {
 	@Test
 	public void testServlet() throws Exception {
 
-		final HttpGet get =
-				new HttpGet("http://localhost:" + port + "/servlet");
+		final HttpGet get = new HttpGet("http://localhost:" + port + "/servlet");
 		final HttpResponse response = client.execute(get);
 
 		final char[] cbuf = new char[10000];
@@ -107,8 +109,8 @@ public class TestHttpServlet {
 	@Test
 	public void testServletAsync() throws Exception {
 
-		final HttpGet get =
-				new HttpGet("http://localhost:" + port + "/servletAsync");
+		final HttpGet get = new HttpGet("http://localhost:" + port
+				+ "/servletAsync");
 		final HttpResponse response = client.execute(get);
 
 		final char[] cbuf = new char[10000];
